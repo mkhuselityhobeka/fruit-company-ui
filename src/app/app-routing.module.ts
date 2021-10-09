1956e03f3d0eba9a0cedf4ae7dfc4a6b95b43228
mkhululityhobeka <mkhuselityhobeka@gmail.com>
2021-10-09 16:26:07 +0200

    created the acreate screen,home screen, login screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import { UserRegistrationComponent } from './user-registration-component/user-registration/user-registration.component';
+import { HomePageComponent } from './home-component/home-page/home-page.component';
+import { UserLoginComponentComponent } from './user-login-component/user-login-component.component';
+const routes: Routes = [
 
-
-const routes: Routes = [];
+  { path: '', redirectTo:"home", pathMatch:'full'},
+  {path: 'home', component:HomePageComponent},
+  {path: 'login', component:UserLoginComponentComponent},
+  {path: 'registration', component:UserRegistrationComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration-component/user-registration/user-registration.component';
+import { HomePageComponent } from './home-component/home-page/home-page.component';
+import { UserLoginComponentComponent } from './user-login-component/user-login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent,
+    HomePageComponent,
+    UserLoginComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
